d3744db367115a5fca49aa2efc260f19faac58a9
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-02-05 13:49:40 -0300

    modulo 1 - parte 6 - validacao e conversao de dados

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.model.Produto;
 import br.com.casadocodigo.loja.model.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -22,17 +28,26 @@ public class ProdutosController {
 	@Autowired
 	private ProdutoDAO produtoDao;
 	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
+	
 	@RequestMapping("/form")
-	public ModelAndView form() {
-		
+	public ModelAndView form() {		
 		ModelAndView modelAndView = new ModelAndView("/produtos/form");
 		modelAndView.addObject("tipos",TipoPreco.values());		
 		return modelAndView;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView grava(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto);
+	public ModelAndView grava(@Valid Produto produto, BindingResult result,  
+			RedirectAttributes redirectAttributes) {	
+		
+		if(result.hasErrors()) {
+			return form();
+		}
+		
 		produtoDao.gravar(produto);		
 		//redireciona com flash atributes pendurando a mensagem na requisicao
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");		
